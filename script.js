ea938d29ce08a5fc19aa803193156b68756d077c
robertoedu <roberto.edu.oliveira@gmail.com>
2021-09-14 01:04:17 -0300

    adicionando paramentros finais, js com comunicação na api, depois de um tempo parado, tive que voltar a fazer para não perder a oportunidade

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var transacao = [];
 var total = 0;
+var prod = [];
 // Mascára monetária finalizando ela, feita com regex
 function formatarMoeda() {
     var elemento = document.getElementById('valor');
     var valor = elemento.value;
 
     valor = valor + '';
-    valor = parseInt(valor.replace(/[\D]+/g, ''));
+    valor = parseFloat(valor.replace(/[\D]+/g, ''));
     valor = valor + '';
     valor = valor.replace(/([0-9]{2})$/g, ",$1");
 
@@ -15,15 +16,17 @@ function formatarMoeda() {
     }
 
     elemento.value = valor;
-    if(valor == 'NaN') elemento.value = '';
-    if(valor == 'null') elemento.value = '';
+    if (valor == 'NaN') elemento.value = '';
+
+    if (valor == 'null') elemento.value = '';
+
 }
 
 // Validação de cada campo do formulário com visualização de cada item, contendo o evento para não recarregar a pafina
-function Validarform(e){
+function Validarform(e) {
     e.preventDefault();
     console.log("Botão clicado");
-   
+
     var escolha = document.getElementById("escolha").value;
     console.log("escolha", escolha);
 
@@ -35,59 +38,84 @@ function Validarform(e){
 
     var exite_erro = false;
 
-    if(escolha == ""){
+    if (escolha == "") {
         exite_erro = true;
         celecalert.innerHTML = "*Selecione a modalidade desejada!";
     }
 
-    if (nome == ""){
+    if (nome == "") {
         exite_erro = true;
         nomealert.innerHTML = "*Preencha com o nome da mercadoria!";
     }
 
-    if(valor == ""){
+    if (valor == "") {
         exite_erro = true;
         valoret.innerHTML = "*Adicione o valor da transição!";
     }
-  // Salvando em Localstorage, em string para poder vizualizar no console, com o recarregamento dos compos inputs, para não precisar ficar apagando
-    if(!exite_erro){
-        if(transacao == null){
-        transacao=[];
+
+    // Salvando em Localstorage, em string para poder vizualizar no console, com o recarregamento dos compos inputs, para não precisar ficar apagando
+    if (!exite_erro) {
+        if (transacao == null) {
+            transacao = [];
         }
 
-        if(escolha == "-"){
-            valor =parseFloat(valor) *-1;
-        }else{
-            valor =valor *1;
+        if (escolha == "-") {
+            valor = parseFloat(valor) * -1;
+        } else {
+            valor = parseFloat(valor) * 1;
         }
-        transacao.push({nome: nome, valor: valor, escolha: escolha});
-        localStorage.setItem("transacao",JSON.stringify(transacao)); 
+        transacao.push({ nome: nome, valor: valor, escolha: escolha });
+        localStorage.setItem("transacao", JSON.stringify(transacao));
         console.log("valor", valor);
         listatransacao();
         document.getElementById("nome").value = "";
         document.getElementById("escolha").value = "";
         document.getElementById("valor").value = "";
-       
+
     }
     return false;
 }
 // listando os produtos cada vez que clicar no botão, fazendo uma lista dinamica
 
-function listatransacao(){
-        transacao = JSON.parse(localStorage.getItem('transacao'));
-        document.getElementById("tjs").innerHTML = "";
-        total = 0;
-        for(let idx_prod in transacao) {
-            total += parseFloat(transacao[idx_prod].valor);
-            document.querySelector('#tjs').innerHTML += 
-                    ` <tr >
-                <td id="sele" >`+transacao[idx_prod].escolha+`</td>
-                <td id="nomer">`+transacao[idx_prod].nome+`</td>
-                <td class="vt" id="vlm"> R$ `+transacao[idx_prod].valor+`</td>
+function listatransacao() {
+    transacao = JSON.parse(localStorage.getItem('transacao'));
+    document.getElementById("tjs").innerHTML = "";
+    total = 0;
+    var Lucro = 'Lucro';
+    var Prejuizo = 'Prejuízo';
+    var Neutro = '';
+    var nem = "Nenhuma transação adicionada"
+    for (let idx_prod in transacao) {
+        total += parseFloat(transacao[idx_prod].valor);
+        document.querySelector('#tjs').innerHTML +=
+            ` <tr >
+                <td id="sele" >`+ transacao[idx_prod].escolha + `</td>
+                <td id="nomer">`+ transacao[idx_prod].nome + `</td>
+                <td class="vt" id="vlm"> R$ `+ transacao[idx_prod].valor + `</td>
             </tr>`
-        }
-        document.getElementById('rs').innerHTML = `
-        <td id="rs" > R$ `+ total+`</td> `;
-        
+    }
+
+    document.getElementById('rs').innerHTML = `
+            <td id="rs" >  `+ total + `</td> `;
+
+
+    if (total > 0) {
+        document.getElementById('lc').innerHTML = `<td colspan="3" class="vt" id="lc">` + Lucro + `</td>`
+    } if (total < 0) {
+        document.getElementById('lc').innerHTML = `<td colspan="3" class="vt" id="lc">` + Prejuizo + `</td>`
+    }
+    else {
+        document.getElementById('lc').innerHTML = `<td colspan="3" class="vt" id="lc">` + Neutro + `</td>`
+    }
+
 }
 
+    fetch('https://api.airtable.com/v0/appRNtYLglpPhv2QD/Historico?maxRecords=3&view=Grid%20view', {
+        headers: {
+            authorization: 'Bearer key2CwkHb0CKumjuM'
+        }
+    }).then((resp) => {
+        return resp.json()
+    }).then((data) =>{
+        console.log(data)
+    })
